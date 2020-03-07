a6f6b6fa80bf51475ea51b077accd4de097365b4
Amelia Urbanowicz <amelia.urbanowicz@gmail.com>
2020-03-07 15:05:52 +0000

    added new users and tasks to seed for mid demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,24 +27,24 @@ Team.create!(
 )
 User.create!(
   password: '123456',
-  email: "a@t.com",
-  first_name: "Amy",
-  last_name: "Tinder",
+  email: "s@o.com",
+  first_name: "Stephanie",
+  last_name: "Opdam",
   manager: true,
   team_id: 1
 )
 User.create!(
   password: '123456',
-  email: "j@b.com",
-  first_name: "James",
-  last_name: "Black",
+  email: "a@u.com",
+  first_name: "Amelia",
+  last_name: "Urbanowicz",
   team_id: 1
 )
 User.create!(
   password: '123456',
-  email: "l@o.com",
-  first_name: "Lily",
-  last_name: "Okman",
+  email: "g@e.com",
+  first_name: "Gigi",
+  last_name: "Etienne",
   team_id: 1
 )
 User.create!(
@@ -56,15 +56,22 @@ User.create!(
 )
 User.create!(
   password: '123456',
-  email: "a@l.com",
-  first_name: "Amy",
-  last_name: "Lucky",
+  email: "t@s.com",
+  first_name: "Tom",
+  last_name: "Smith",
+  team_id: 1
+)
+User.create!(
+  password: '123456',
+  email: "j@b.com",
+  first_name: "James",
+  last_name: "Black",
   manager: true,
   team_id: 2
 )
 Task.create!(
   title: "Hire a replacement for Alex",
-  description: "Before he leaves on the 24th of March",
+  description: "Before he leaves on the 24th of March.",
   deadline: '2020-03-24',
   etc: 5,
   urgent: true,
@@ -73,7 +80,7 @@ Task.create!(
   )
 Task.create!(
   title: "Send contracts to Ashurst",
-  description: "Contracts need to be sent ASAP to John from marketing at Ashurst",
+  description: "Contracts need to be sent ASAP to John from marketing at Ashurst.",
   deadline: '2020-03-20',
   etc: 15,
   urgent: true,
@@ -82,24 +89,69 @@ Task.create!(
   )
 Task.create!(
   title: "Write press release for Trippify",
-  description: "Launch is on the 15th",
-  deadline: '2020-03-15',
+  description: "Launch is on the 15th.",
+  deadline: '2020-03-18',
   etc: 2,
   urgent: true,
   team_id: 1,
-  user_id: 2
+  user_id: 1
   )
 Task.create!(
-  title: "Write press release for Trippify",
-  description: "Launch is on the 15th",
+  title: "Create minutes - last team meeting",
+  description: "Updates on recruitment for Alex's post.",
+  deadline: '2020-03-17',
+  etc: 2,
+  urgent: true,
+  team_id: 1,
+  user_id: 1
+  )
+Task.create!(
+  title: "Prepare meeting room 1 for the Board Meeting @3PM",
+  description: "Water, coffee, tea, snacks, fruits, cups, plates, and napkins. Connect Amy's laptop to the screen.",
   deadline: '2020-03-15',
   etc: 2,
   urgent: true,
   team_id: 1
   )
 Task.create!(
-  title: "Task for team 2",
-  description: "Launch is on the 15th",
+  title: "Send invoices to SuperCycle",
+  description: "Launching the website.",
+  deadline: '2020-03-12',
+  etc: 2,
+  urgent: true,
+  team_id: 1,
+  user_id: 2
+  )
+Task.create!(
+  title: "Post ad for a new role on indeed.com",
+  description: "Ask Amy for the sign off.",
+  deadline: '2020-03-24',
+  etc: 2,
+  urgent: true,
+  team_id: 1,
+  user_id: 2
+  )
+Task.create!(
+  title: "Arrange meeting with new client - Bolt",
+  description: "Prepare presentation on 'How to beat Uber'.",
+  deadline: '2020-03-15',
+  etc: 2,
+  urgent: true,
+  team_id: 1,
+  user_id: 3
+  )
+Task.create!(
+  title: "Arrange staff lunch delivery from BunBunBun on Mondays",
+  description: "Ask staff for allergies and meat/vegetarian/vegan options.",
+  deadline: '2020-03-21',
+  etc: 2,
+  urgent: true,
+  team_id: 1,
+  user_id: 4
+  )
+Task.create!(
+  title: "Organise team meeting on the 15th",
+  description: "Discuss recent developments and introduce new team members.",
   deadline: '2020-03-15',
   etc: 2,
   urgent: true,
