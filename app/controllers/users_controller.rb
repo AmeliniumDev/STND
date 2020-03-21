f859d606725cf13ea3c1560502eb0d98b935ea34
Amelia Urbanowicz <amelia.urbanowicz@gmail.com>
2020-03-21 15:07:50 +0000

    individual user tasks ordered

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @tasks = @user.tasks.where(complete: false)
+    @tasks = @user.tasks.where(complete: false).reorder('deadline ASC', 'urgent DESC', 'etc ASC')
     @team = @user.team
     @completed_tasks = Task.where(team: @team, complete: true, user: @user).reorder('deadline ASC', 'urgent DESC', 'etc ASC')
   end
