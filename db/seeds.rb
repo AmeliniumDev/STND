7ed05e3a0dc753887af4e4f4270546ad59fb5e36
Gigi Etienne <g.etienne.rios@gmail.com>
2020-03-21 13:13:56 +0000

    finished seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Clean the database"
 
-Company.all.destroy_all
-Team.all.destroy_all
-User.all.destroy_all
 Task.all.destroy_all
+User.all.destroy_all
+Team.all.destroy_all
+Company.all.destroy_all
 
 puts "Seed the database"
 
@@ -32,7 +32,6 @@ User.create!(
   email: "s@o.com",
   first_name: "Stephanie",
   last_name: "Opdam",
-  manager: true,
   team_id: 1
 )
 User.create!(
@@ -47,6 +46,7 @@ User.create!(
   email: "g@e.com",
   first_name: "Gigi",
   last_name: "Etienne",
+  manager: true,
   team_id: 1
 )
 User.create!(
@@ -74,29 +74,9 @@ User.create!(
 )
 # TASKS - team 1
 Task.create!(
-  title: "Hire a replacement for Alex",
-  description: "Before he leaves on the 24th of March.",
-  deadline: '2020-03-24',
-  hours: 5,
-  minutes: 15,
-  urgent: true,
-  team_id: 1,
-  user_id: 1
-  )
-Task.create!(
-  title: "Send contracts to Ashurst",
-  description: "Contracts need to be sent ASAP to John from marketing at Ashurst.",
-  deadline: '2020-03-25',
-  hours: 3,
-  minutes: 30,
-  urgent: false,
-  team_id: 1,
-  user_id: 1
-  )
-Task.create!(
-  title: "Write press release for Trippify",
-  description: "Launch is on the 25th.",
-  deadline: '2020-03-25',
+  title: "Write Tata Motors press release",
+  description: "Tata team to review on Friday 27th March.",
+  deadline: '2020-03-21',
   hours: 2,
   minutes: 0,
   urgent: false,
@@ -104,48 +84,68 @@ Task.create!(
   user_id: 1
   )
 Task.create!(
-  title: "Create minutes - board meeting",
-  description: "Updates on recruitment for Alex's post.",
-  deadline: '2020-03-23',
-  hours: 1,
-  minutes: 45,
+  title: "Organise branding assets in folders",
+  description: "For sales team",
+  deadline: '2020-03-21',
+  hours: 5,
+  minutes: 30,
   urgent: false,
   team_id: 1,
-  user_id: 1
+  user_id: 2
+  )
+Task.create!(
+  title: "Talk to the BBC about Whiz",
+  description: "We have to prep them for the interview with James from Whiz next week",
+  deadline: '2020-03-21',
+  hours: 1,
+  minutes: 0,
+  urgent: false,
+  team_id: 1,
+  user_id: 3
+  )
+Task.create!(
+  title: "Write minutes for management board meeting",
+  description: "Alex wants these detailed notes",
+  deadline: '2020-03-21',
+  hours: 1,
+  minutes: 30,
+  urgent: false,
+  team_id: 1,
+  user_id: 4
   )
 Task.create!(
   title: "Prepare meeting room for 1 pm",
   description: "Water, coffee, tea, snacks, fruits, cups, plates, and napkins. Connect Amy's laptop to the screen.",
-  deadline: '2020-03-23',
-  hours: 7,
-  minutes: 0,
+  deadline: '2020-03-21',
+  hours: 0,
+  minutes: 30,
   urgent: false,
-  team_id: 1
+  team_id: 5
   )
 Task.create!(
   title: "Send invoices to SuperCycle",
   description: "Launching the website.",
   deadline: '2020-03-23',
-  hours: 2,
+  hours: 1,
   minutes: 0,
-  urgent: true,
-  team_id: 1,
-  user_id: 2
-  )
-Task.create!(
-  title: "Post ad for a new role on indeed.com",
-  description: "Ask Amy for the sign off.",
-  deadline: '2020-03-24',
-  hours: 0,
-  minutes: 15,
   urgent: false,
   team_id: 1,
   user_id: 2
   )
 Task.create!(
+  title: "Post ad for a new role on indeed.com",
+  description: "Marketing Assistant role",
+  deadline: '2020-03-21',
+  hours: 1,
+  minutes: 15,
+  urgent: false,
+  team_id: 1,
+  )
+
+Task.create!(
   title: "Arrange meeting with new client - Bolt",
   description: "Prepare presentation on 'How to beat Uber'.",
-  deadline: '2020-03-23',
+  deadline: '2020-03-22',
   hours: 0,
   minutes: 15,
   urgent: false,
@@ -155,7 +155,7 @@ Task.create!(
 Task.create!(
   title: "Arrange staff lunch delivery from BunBunBun on Mondays",
   description: "Ask staff for allergies and meat/vegetarian/vegan options.",
-  deadline: '2020-03-26',
+  deadline: '2020-03-23',
   hours: 1,
   minutes: 30,
   urgent: false,
@@ -163,19 +163,20 @@ Task.create!(
   user_id: 4
   )
 Task.create!(
-  title: "Draft April's strategy",
-  description: "Prepare a draft of the teams plan/strategy for April prior to Dominic's phone call",
-  deadline: '2020-03-25',
+  title: "Create marketing materials for client Triumph",
+  description: "They need callouts with motorcyles and social media posts content",
+  deadline: '2020-03-21',
   hours: 1,
   minutes: 00,
   urgent: false,
   team_id: 1,
-  user_id: 1
+  user_id: 2
   )
+
 Task.create!(
   title: "Conference call with Dominic",
-  description: "Setup conference call with Dominic to discuss April's strategy",
-  deadline: '2020-03-27',
+  description: "Conference call with Dominic to discuss April's strategy",
+  deadline: '2020-03-22',
   hours: 2,
   minutes: 00,
   urgent: false,
@@ -185,7 +186,7 @@ Task.create!(
 Task.create!(
   title: "Final version of April's strategy",
   description: "Merge with Dominic's, send it out to the board of directors and team once approved.",
-  deadline: '2020-03-27',
+  deadline: '2020-03-22',
   hours: 1,
   minutes: 30,
   urgent: false,
@@ -195,7 +196,7 @@ Task.create!(
 Task.create!(
   title: "QC the contract for New River",
   description: "Send for signature to Kathryn",
-  deadline: '2020-03-27',
+  deadline: '2020-03-22',
   hours: 1,
   minutes: 00,
   urgent: false,
@@ -203,9 +204,9 @@ Task.create!(
   user_id: 2
   )
 Task.create!(
-  title: "Add new clients to the database",
+  title: "Add this week's new clients to the database",
   description: "Speedy Equipment, Handmade Sandwich, Soho Socks",
-  deadline: '2020-03-27',
+  deadline: '2020-03-22',
   hours: 1,
   minutes: 00,
   urgent: false,
@@ -215,7 +216,7 @@ Task.create!(
 Task.create!(
   title: "Speedy Equipment - ad strategy",
   description: "Discuss with Karen the latest developments",
-  deadline: '2020-03-27',
+  deadline: '2020-03-22',
   hours: 1,
   minutes: 30,
   urgent: false,
@@ -223,9 +224,9 @@ Task.create!(
   user_id: 4
   )
 Task.create!(
-  title: "Speedy Equipment - arrange meeting",
+  title: "Speedy Equipment - meeting",
   description: "To discuss initial ad ideas and branding",
-  deadline: '2020-03-30',
+  deadline: '2020-03-22',
   hours: 1,
   minutes: 30,
   urgent: false,
@@ -233,9 +234,9 @@ Task.create!(
   user_id: 4
   )
 Task.create!(
-  title: "Handmade Sandwich - food testing",
-  description: "Know what you are selling! #best_job_ever",
-  deadline: '2020-03-30',
+  title: "Design new logo for Soho Socks",
+  description: "See below for their brief: We want a new logo with our 3 key colors that is simpler and attracts more attention but remains minimalistic",
+  deadline: '2020-03-22',
   hours: 1,
   minutes: 30,
   urgent: false,
@@ -243,10 +244,10 @@ Task.create!(
   user_id: 3
   )
 Task.create!(
-  title: "Soho Socks - colorful life",
+  title: "Soho Socks posters",
   description: "Choose color palette for the new Soho Socks poster",
-  deadline: '2020-03-30',
-  hours: 4,
+  deadline: '2020-03-22',
+  hours: 1,
   minutes: 00,
   urgent: false,
   team_id: 1,
@@ -255,7 +256,7 @@ Task.create!(
 Task.create!(
   title: "Sales reports - March",
   description: "Prepare team sales reports for March and send to the Finance Team",
-  deadline: '2020-04-01',
+  deadline: '2020-03-23',
   hours: 4,
   minutes: 00,
   urgent: true,
@@ -265,7 +266,7 @@ Task.create!(
 Task.create!(
   title: "Monthly team meeting",
   description: "April strategy, recent developments, introduce new team members.",
-  deadline: '2020-04-02',
+  deadline: '2020-04-01',
   hours: 1,
   minutes: 30,
   urgent: false,
@@ -274,14 +275,58 @@ Task.create!(
   )
 # TASKS - team 2
 Task.create!(
-  title: "Team 2 Task",
-  description: "Team 2 task description",
-  deadline: '2020-03-26',
+  title: "Make video for Mercedes-Benz",
+  description: "They want a new video for their new MBUX car, they've sent a very detailed script for what they want, we just need to review and execute",
+  deadline: '2020-03-24',
   hours: 0,
   minutes: 15,
   urgent: false,
   team_id: 2,
   user_id: 1
+  )
+
+Task.create!(
+  title: "Allocate the daily tasks to the team",
+  description: "Make sure no one has too little to do or too many tasks",
+  deadline: '2020-03-21',
+  hours: 0,
+  minutes: 15,
+  urgent: true,
+  team_id: 2,
+  user_id: 3
+  )
+
+Task.create!(
+  title: "Write peer review for Anna",
+  description: "To determine is she passes her probation",
+  deadline: '2020-03-21',
+  hours: 0,
+  minutes: 15,
+  urgent: false,
+  team_id: 2,
+  user_id: 2
+  )
+
+Task.create!(
+  title: "Post the Soho Socks posters to their office",
+  description: "Their address is 65 Nomad Road, NW1 5EU",
+  deadline: '2020-03-21',
+  hours: 0,
+  minutes: 30,
+  urgent: false,
+  team_id: 2,
+  user_id: 1
+  )
+
+Task.create!(
+  title: "Review Mercedes-Benz script for their new video ad",
+  description: "Have it approved by Ben before starting on it",
+  deadline: '2020-03-21',
+  hours: 1,
+  minutes: 0,
+  urgent: false,
+  team_id: 2,
+  user_id: 3
   )
 
 puts "All done. Happy coding :)"
